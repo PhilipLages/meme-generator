4c25918469e3d7d86151c41b9087dc64ac8c20cc
Philip Lages <psilvaelages@gmail.com>
2022-06-05 11:56:49 -0300

    Refactor code and Lint Fix

--- script.js
+++ script.js
@@ -1,59 +1,51 @@
-function createNewTag(tag, element, value) {         
-  const createTag = document.createElement(tag);        
-  createTag.setAttribute(element, value);    
-  return createTag;
-}
-
 const memeText = document.getElementById('meme-text');
 const input = document.getElementById('text-input');
 const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
+const memeImageContainer = document.getElementById('meme-image-container');
 
 function createMemeText() {
-  input.addEventListener('keyup', function() {    
-    memeText.innerText = input.value;    
-  })
+  input.addEventListener('keyup', () => {
+    memeText.innerText = input.value;
+  });
 }
 createMemeText();
 
-// Código baseado na explicação feita em : https://medium.com/@iamcodefoxx/how-to-upload-and-preview-an-image-with-javascript-749b92711b91
-memeInsert.addEventListener('change', function() {
-  const reader = new FileReader();
-  reader.addEventListener('load', function() {
-    const selectedImage = reader.result;    
-    memeImage.style.backgroundImage = `url(${selectedImage})`;
-  });
-  reader.readAsDataURL(this.files[0]);
+memeInsert.addEventListener('change', (e) => {
+  memeImage.src = URL.createObjectURL(e.target.files[0]);
+  memeImage.onload = function () {
+    URL.revokeObjectURL(memeImage.src);
+  };
 });
 
 const fire = document.getElementById('fire');
-fire.addEventListener('click', function() {
-  const memeImageContainer = document.getElementById('meme-image-container');
+fire.addEventListener('click', () => {
+  memeImageContainer.style.border = ' ';
   memeImageContainer.style.border = '3px dashed rgb(255, 0, 0)';
 });
 
 const water = document.getElementById('water');
-water.addEventListener('click', function() {
-  const memeImageContainer = document.getElementById('meme-image-container');
+water.addEventListener('click', () => {
   memeImageContainer.style.border = ' ';
   memeImageContainer.style.border = '5px double rgb(0, 0, 255)';
 });
 
 const earth = document.getElementById('earth');
-earth.addEventListener('click', function() {
-  const memeImageContainer = document.getElementById('meme-image-container');
+earth.addEventListener('click', () => {
+  memeImageContainer.style.border = ' ';
   memeImageContainer.style.border = '6px groove rgb(0, 128, 0)';
 });
 
 const memes = document.getElementsByClassName('meme');
 
-function changeMemeImage(e){
+function changeMemeImage(e) {
   memeImage.src = e.target.src;
 }
 
 function addMemesEvents() {
-  for (let meme of memes) {
-    meme.addEventListener('click', changeMemeImage);
+  for (let meme = 0; meme < memes.length; meme += 1) {
+    const memeEvent = memes[meme];
+    memeEvent.addEventListener('click', changeMemeImage);
   }
 }
 addMemesEvents();
